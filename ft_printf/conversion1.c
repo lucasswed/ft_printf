28fe4b1a443864f24bbef4d6ce533df2d5e06f1c
Lucas filipe Martins da silva <lucas-ma@c1r18s11.42lisboa.com>
2021-11-02 15:54:18 +0000

    ah

--- ft_printf/conversion1.c
+++ ft_printf/conversion1.c
@@ -6,8 +6,25 @@
 /*   By: lucas-ma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 15:27:23 by lucas-ma          #+#    #+#             */
-/*   Updated: 2021/11/02 15:29:51 by lucas-ma         ###   ########.fr       */
+/*   Updated: 2021/11/02 15:53:53 by lucas-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 
+int	ft_putchar_fd(int c, int fd)
+{
+	return (write(fd, &(char)c, 1));
+}
+
+int	ft_putstr_fd(char *str, int fd)
+{
+	int	index;
+
+	index = 0;
+	while (str[index])
+		index += ft_putchar(str[index], fd);
+	return (index);
+}
+
+int	ft_putaddr_fd(
