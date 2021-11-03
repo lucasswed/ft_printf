48abe7d7b56a36df72d8cd6caaaa9901711e03df
Lucas filipe Martins da silva <lucas-ma@c1r17s9.42lisboa.com>
2021-11-03 12:31:25 +0000

    almost

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,40 +6,64 @@
 /*   By: lucas-ma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 10:24:47 by lucas-ma          #+#    #+#             */
-/*   Updated: 2021/11/02 11:05:00 by lucas-ma         ###   ########.fr       */
+/*   Updated: 2021/11/03 12:22:49 by lucas-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(char *fmt, ...)
+static int	ft_conversion(const char **fmt, va_list ap)
+{
+	if (**fmt == 'c')
+		return (ft_putchar_fd(va_arg(ap, int), 1));
+	if (**fmt == 's')
+		return (ft_putstr_fd(va_arg(ap, char *), 1));
+	if (**fmt == 'p')
+		return (ft_putaddr_fd(va_arg(ap, unsigned int), 1));
+	if (**fmt == 'd' || **fmt == 'i')
+		return (ft_putnbr_fd(va_arg(ap, int), 1));
+	if (**fmt == 'u')
+		return (ft_putunbr_fd(va_arg(ap, unsigned int), 1));
+	if (**fmt == 'x')
+		return (ft_printhexa(va_arg(ap, unsigned int), 1));
+	if (**fmt == 'X')
+		return (ft_printhexam(va_arg(ap, unsigned int), 1));
+	return (1);
+}
+
+static int	ft_printf_main(va_list	ap, const char *fmt)
+{
+	int	index;
+	int	count;
+
+	index = 0;
+	count = 0;
+	while (fmt[index])
+	{
+		if (fmt[index] == '%')
+		{
+			index++;
+			if (ft_isconv(fmt[index], "cspdiuxX"))
+				count += ft_conversion(&fmt, ap);
+			else
+				count += ft_putchar_fd(fmt[index], 1);
+		}
+		index++;
+		count++;
+	}
+	return (count);
+}
+
+int	ft_printf(const char *fmt, ...)
 {
 	va_list	ap;
-	int		i;
+	int		count;
 
-	i = 0;
+	if (!fmt)
+		return (-1);
+	count = 0;
 	va_start(ap, fmt);
-	while (*fmt)
-	{
-		if (*fmt == '%')
-		{
-			if (*(fmt + 1) == 'c')
-				ft_print_char(va_arg(ap, int));
-			else if (*(fmt + 1) == 's')
-				ft_print_string(va_arg(ap, char *));
-			else if (*(fmt + 1) == 'd' || *(fmt + 1) == 'i')
-				ft_print_number(va_arg(ap, int));
-			else if (*(fmt + 1) == 'u')
-				ft_print_unsigned(va_arg(ap, unsigned int));
-			else if (*(fmt + 1) == 'x' || *(fmt + 1) == 'X')
-				ft_print_hexa(va_arg(ap, int));
-			else if (*(fmt + 1) == 'p')
-				ft_print_void(va_arg(ap, void *));
-			else if (*(fmt + 1) == '%')
-				ft_putchar_fd('%', 1);
-		}
-		ft_putchar_fd(*fmt, 1);
-		i++;
-	}
-	return (i);
+	count = ft_printf_main(ap, fmt);
+	va_end(ap);
+	return (count);
 }
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,11 +6,24 @@
 /*   By: lucas-ma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 12:15:47 by lucas-ma          #+#    #+#             */
-/*   Updated: 2021/11/02 10:24:38 by lucas-ma         ###   ########.fr       */
+/*   Updated: 2021/11/03 12:23:01 by lucas-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# include <stdarg.h>
+# include <unistd.h>
 
+int	ft_printf(const char *fmt, ...);
+int	ft_putchar_fd(char c, int fd);
+int	ft_putstr_fd(char *str, int fd);
+int	ft_putaddr_fd(unsigned int addr, int fd);
+int	ft_putunbr_fd(int nb, int fd);
+int	ft_putnbr_fd(int nb, int fd);
+int	ft_printhexa(unsigned int nb, int fd);
+int	ft_printhexam(unsigned int nb, int fd);
+int	ft_isconv(char c, char *str);
+
+#endif
